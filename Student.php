85aa6145a451c7c7b667c749a8d6a1cd80b30bd8
Lancelei <lancelei.h@hotmail.ca>
2017-09-08 12:05:02 -0700

    Can add array of grades of Students and calculate average of grades.

--- Student.php
+++ Student.php
@@ -23,5 +23,19 @@ class Student {
     function add_email($which,$address) {
         $this->emails[$which] = $address;
     }
+    
+    
+    function add_grade($grade) {
+        $this->grades[] = $grade;
+    }
+    
+    function average() {
+        $total = 0;
+        foreach ($this->grades as $value)
+            $total += $value;
+        return $total / count($this->grades);
+    }
+    
+    
 
 }
