4df0246021c0c26521d9c78f3985f14d977ff0b8
Lancelei <lancelei.h@hotmail.ca>
2017-09-08 13:08:33 -0700

    Added comments inside Student.php

--- Student.php
+++ Student.php
@@ -13,6 +13,15 @@
  */
 class Student {
     
+    /**
+     * _construct
+     * 
+     * Constructs a Student object.
+     * 
+     * Author: Lancelei Herradura
+     * Date:   August 8, 2017
+     * 
+     */
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -20,15 +29,46 @@ class Student {
         $this->grades = array();
     }
     
+    /**
+     * add_email
+     * 
+     * This function adds an email address that the student goes by.
+     * The email address is accompanied by for which it is used for.
+     * 
+     * Author: Lancelei Herradura
+     * Date:   August 8, 2017
+     * 
+     * @param string $which email category
+     * @param string $address email address
+     */
     function add_email($which,$address) {
         $this->emails[$which] = $address;
     }
     
-    
+    /**
+     * add_grade
+     * 
+     * This function adds a grade to the Student's array of grades.
+     * 
+     * Author: Lancelei Herradura
+     * Date:   August 8, 2017
+     * 
+     * @param integer $grade grade
+     */
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    /**
+     * average
+     * 
+     * Returns the calculated average of the Student's grades.
+     * 
+     * Author: Lancelei Herradura
+     * Date: August 8, 2017
+     * 
+     * @return integer calculated average
+     */
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -36,6 +76,17 @@ class Student {
         return $total / count($this->grades);
     }
     
+    /**
+     * toString
+     * 
+     * Returns a string representation of the Student's details in
+     * pretty format.
+     * 
+     * Author: Lancelei Herradura
+     * Date: August 8, 2017
+     * 
+     * @return string
+     */
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
@@ -44,5 +95,6 @@ class Student {
         $result .= "\n";
         return '<pre>'.$result.'</pre>';
     }
+    
 
 }
